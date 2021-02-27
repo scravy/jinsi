58dee08f0ea7b7fde77348c5dc330afccdcb832c
Julian Fleischer <tirednesscankill@warhog.net>
2021-02-27 05:00:37 +0100

    Export Functions

--- jinsi/__init__.py
+++ jinsi/__init__.py
@@ -1,3 +1,4 @@
+from .__pkginfo__ import version as __version__
 from .api import \
     load_file, \
     load_string, \
@@ -7,11 +8,10 @@ from .api import \
     render_string, \
     render1f, \
     render1s
-from .util import cached_method, cached_function
+from .functions import Functions
 from .jsonutil import loadjson, loadjson_all, dumpjson
+from .util import cached_method, cached_function
 from .yamlutil import loadyaml, loadyaml_all, dumpyaml
-
-from .__pkginfo__ import version as __version__
 
 __all__ = [
     'load_file',
@@ -22,6 +22,8 @@ __all__ = [
     'render_string',
     'render1f',
     'render1s',
+
+    'Functions',
 
     'loadjson',
     'loadjson_all',
